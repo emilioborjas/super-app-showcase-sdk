bf6739788a98870e37b5b0cf3d0102fc3eda0bfd
“emilioborjas” <emilio.borjas@rimac.com.pe>
2025-07-03 10:02:10 -0500

    first commit

---
+++ sdk/lib/rnxPresent.js
@@ -0,0 +1,37 @@
+/**
+* Creates a final dependency preset to be used
+* in the host/mini-apps by doing the following:
+* - adds depenendencies from dependencies.json and devDependencies.j
+son
+* - adds the SDK as a dev dependency
+* - adds the `super-app` as a capability
+*/
+const addSdkCapabilities = (dependencies, devDependencies) => {
+    const path = require("path");
+    const sdkPackagePath = path.resolve(__dirname,"..", "package.json");
+    const sdkPackageJson = require(sdkPackagePath);
+
+    const profile = {
+        ...dependencies,
+        ...devDependencies,
+        "@your-org/super-app-showcase-sdk": {
+            name: "@your-org/super-app-showcase-sdk",
+            version: sdkPackageJson.version,
+            devOnly: true,
+        },
+    };
+
+    return Object.assign(profile, {
+        "super-app": {
+        name: "#meta",
+        apabilities: Object.keys(profile),
+        },
+    });
+};
+
+module.exports = {
+    main: addSdkCapabilities(
+        require("./dependencies.json"),
+        require("./devDependencies.json")
+    ),
+};
